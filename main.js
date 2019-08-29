2d86306c002ef2c1cc1f0c6634ee37fbf8ddce31
temnui <temnui@gmail.com>
2019-08-29 13:38:04 +0300

    cleanUp first (not complete)

--- main.js
+++ main.js
@@ -4,11 +4,13 @@ let electron = require('electron');
 let XLSX = require('xlsx');
 let app = electron.app;
 
+require('electron-reload')(__dirname);
+
 let win = null;
 
 function createWindow() {
 	if(win) return;
-	win = new electron.BrowserWindow({width:800, height:600});
+	win = new electron.BrowserWindow({width:1200, height:800});
 	win.loadURL("file://" + __dirname + "/index.html");
 	win.webContents.openDevTools();
 	win.on('closed', function() { win = null; });
